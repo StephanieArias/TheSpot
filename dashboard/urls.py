330b446f76b96a20df37d9127265a96eaa924fc6
StephanieArias <72588483+StephanieArias@users.noreply.github.com>
2021-03-09 03:03:47 -0800

    Search
    Search with ticketmaster api and google maps setup.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,14 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.dashboard),
+    path('about', views.about,  name='about'),
+    path('search', views.search_form, name='search_form'),
+    path('concerts_list', views.concerts_list, name='concerts_list'),
     path('LoginHome', views.LoginHome),
-    path('register', views.register),
-    path('login', views.login),
-    path('logout', views.logout),
-    path('success', views.success),
+    # path('register', views.register),
+    # path('login', views.login),
+    # path('logout', views.logout),
+    # path('success', views.success),
 ]
